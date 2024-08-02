3bd54dc1f2131a9b3169cce91e509bd235cee7b5
yuanshang000qq <157210311+yuanshang000qq@users.noreply.github.com>
2024-08-03 01:03:44 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 import paramiko
 import requests
 import json
+import logging
 from datetime import datetime, timezone, timedelta
 
 def ssh_multiple_connections(hosts_info, command):
@@ -20,8 +21,10 @@ def ssh_multiple_connections(hosts_info, command):
             users.append(user)
             hostnames.append(hostname)
             ssh.close()
+            logger.info("运行状态: %s",f"用户：{username}，连接 {hostname} 成功")
         except Exception as e:
-            print(f"用户：{username}，连接 {hostname} 时出错: {str(e)}")
+            logger.info("运行状态: %s",f"用户：{username}，连接 {hostname} 时出错: {str(e)}")
+            #print(f"用户：{username}，连接 {hostname} 时出错: {str(e)}")
     return users, hostnames
 
 ssh_info_str = os.getenv('SSH_INFO', '[]')
@@ -35,50 +38,29 @@ for user, hostname in zip(user_list, hostname_list):
     content += f"用户名：{user}，服务器：{hostname}\n"
 beijing_timezone = timezone(timedelta(hours=8))
 time = datetime.now(beijing_timezone).strftime('%Y-%m-%d %H:%M:%S')
-menu = requests.get('https://api.zzzwb.com/v1?get=tg').json()
 loginip = requests.get('https://api.ipify.org?format=json').json()['ip']
 content += f"本次登录用户共： {user_num} 个\n登录时间：{time}\n登录IP：{loginip}"
 
 push = os.getenv('PUSH')
 
-def mail_push(url):
-    data = {
-        "body": content,
-        "email": os.getenv('MAIL')
+def post_weichat_2():
+    url = 'http://www.pushplus.plus/send'
+    # 从环境变量中获取token
+    token = os.getenv('PUSHPLUS_TOKEN')
+    if not token:
+        logger.error("PUSHPLUS_TOKEN is not set.")
+        return
+
+    # post发送的字典参数
+    data_dict = {
+        'token': token,  # 使用环境变量中的token值
+        'title': 'serv00保活签到',
+        'template': 'txt',
+        'content': content  # 确保msg变量在此处之前已经被定义
     }
+    r = requests.post(url, data=data_dict)  # 发起请求
+    print(r.text)
+    logger.info("推送状态: %s", r.text)
 
-    response = requests.post(url, json=data)
-
-    try:
-        response_data = json.loads(response.text)
-        if response_data['code'] == 200:
-            print("推送成功")
-        else:
-            print(f"推送失败，错误代码：{response_data['code']}")
-    except json.JSONDecodeError:
-        print("连接邮箱服务器失败了")
-
-def telegram_push(message):
-    url = f"https://api.telegram.org/bot{os.getenv('TELEGRAM_BOT_TOKEN')}/sendMessage"
-    payload = {
-        'chat_id': os.getenv('TELEGRAM_CHAT_ID'),
-        'text': message,
-        'parse_mode': 'HTML',
-        'reply_markup': json.dumps({
-            "inline_keyboard": menu,
-            "one_time_keyboard": True
-         })
-    }
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    if response.status_code != 200:
-        print(f"发送消息到Telegram失败: {response.text}")
-
-if push == "mail":
-    mail_push('https://zzzwb.us.kg/test')
-elif push == "telegram":
-    telegram_push(content)
-else:
-    print("推送失败，推送参数设置错误")
+if __name__ == '__main__':
+    post_weichat_2()
