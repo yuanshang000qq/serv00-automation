b4acfa54764e34d4140698968a7fb1d56b047dae
yuanshang000qq <157210311+yuanshang000qq@users.noreply.github.com>
2024-08-08 01:25:43 +0800

    Update run.py

--- run.py
+++ run.py
@@ -43,28 +43,55 @@ loginip = requests.get('https://api.ipify.org?format=json').json()['ip']
 content += f"本次登录用户共： {user_num} 个\n登录时间：{time}\n登录IP：{loginip}"
 
 
-def post_weichat_2():
-    token = os.getenv('ANPUSH_TOKEN')
-    if not token:
-        logger.error("ANPUSH_TOKEN is not set.")
-        return
+# def post_weichat_2():
+#     token = os.getenv('ANPUSH_TOKEN')
+#     if not token:
+#         logger.error("ANPUSH_TOKEN is not set.")
+#         return
 
-    url = "https://api.anpush.com/push/"+token
-    # 从环境变量中获取token
+#     url = "https://api.anpush.com/push/"+token
+#     # 从环境变量中获取token
     
    
-    # post发送的字典参数
-    data_dict = {
-        'title': 'serv00保活签到',
-        'content': content,  # 确保变量在此处之前已经被定义
-        "channel": "56466"
-    }
-    headers = {
-    "Content-Type": "application/x-www-form-urlencoded"
-    }
-    r = requests.post(url, headers=headers, data=data_dict)  # 发起请求
-    print(r.text)
-    logger.info("推送状态: %s", r.text)
+#     # post发送的字典参数
+#     data_dict = {
+#         'title': 'serv00保活签到',
+#         'content': content,  # 确保变量在此处之前已经被定义
+#         "channel": "56466"
+#     }
+#     headers = {
+#     "Content-Type": "application/x-www-form-urlencoded"
+#     }
+#     r = requests.post(url, headers=headers, data=data_dict)  # 发起请求
+#     print(r.text)
+#     logger.info("推送状态: %s", r.text)
 
-if __name__ == '__main__':
-    post_weichat_2()
+# if __name__ == '__main__':
+#     post_weichat_2()
+
+def send_webhook(content):
+    token = os.getenv('WEBHOOK')
+    webhook_url = token  # 请在这里填入你的key
+    headers = {'Content-Type': 'application/json'}
+    payload = {
+        "msgtype": "text",
+        "text": {
+            "content": content
+        }
+    }
+
+    try:
+        response = requests.post(webhook_url, json=payload, headers=headers)
+        if response.json().get('errcode') == 0:
+            return response.json()  # 如果请求成功，返回响应数据
+        else:
+            raise Exception(response.json().get('errmsg', '发送失败'))
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+# 使用函数示例
+content = "这里是你想要发送的消息内容"
+result = send_webhook(content)
+print(result)
+logger.info("推送状态: %s", result)
